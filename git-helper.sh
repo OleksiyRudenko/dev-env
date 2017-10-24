0ae596d57af2206116b4f1814d6bb62b90bf6d86
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2017-10-24 21:13:45 +0300

    add git config

--- git-helper.sh
+++ git-helper.sh
@@ -3,5 +3,7 @@ git config --global user.email oleksiy.rudenko@gmail.com
 git config --global alias.logg "log --graph --full-history --all --color --pretty=format:\"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s\""
 git config --global core.editor "'D:/Program Files (x86)/Notepad++/notepad++.exe' -multiInst -notabbar -nosession -noPlugin '$*'"
 git config --global core.pager cat
+# git config --global push.default upstream
+git config --global merge.conflictstyle diff3
 git town alias true
 git config git-town.code-hosting-driver github
