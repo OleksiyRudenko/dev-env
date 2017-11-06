a97c20c01d1b1a3fa2fd27e02cd08029ca2e9637
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2017-11-06 20:03:25 +0200

    add more git config

--- git-helper.sh
+++ git-helper.sh
@@ -1,7 +1,12 @@
 git config --global user.name OleksiyRudenko
 git config --global user.email oleksiy.rudenko@gmail.com
 git config --global alias.logg "log --graph --full-history --all --color --pretty=format:\"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s\""
+git config --global format.commitMessageColumns 72
 git config --global core.editor "'D:/Program Files (x86)/Notepad++/notepad++.exe' -multiInst -notabbar -nosession -noPlugin '$*'"
 git config --global core.pager cat
+git config --global color.ui true
+git config --global merge.conflictstyle diff3
+# Windows specific
+git config --system core.longpaths true
 git town alias true
 git config git-town.code-hosting-driver github
